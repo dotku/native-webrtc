d955f395f89e9187c86dc2f36d104f50d4c9c661
Weijing Lin <weijingjaylin@gmail.com>
2020-09-11 04:25:06 -0700

    use https for server

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
+const cors = require('cors');
 const express = require("express");
+const fs = require('fs');
 const http = require("http");
+const https = require('https');
+const options = {
+    key: fs.readFileSync('client/.cert/key.pem'),
+    cert: fs.readFileSync('client/.cert/cert.pem')
+};
+
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(options, app);
 const socket = require("socket.io");
 const io = socket(server);
 
 const rooms = {};
-
+app.use(cors());
+app.get("/", (req, res) => {
+    res.send("hello, starnger, let's get to know each other.");
+})
+app.get("/rooms", (req, res) => {
+    res.send({
+        rooms
+    })
+})
 io.on("connection", socket => {
     socket.on("join room", roomID => {
+        // console.log("join room");
         if (rooms[roomID]) {
             rooms[roomID].push(socket.id);
         } else {
@@ -22,14 +39,17 @@ io.on("connection", socket => {
     });
 
     socket.on("offer", payload => {
+        // console.log("offer");
         io.to(payload.target).emit("offer", payload);
     });
 
     socket.on("answer", payload => {
+        // console.log("answer");
         io.to(payload.target).emit("answer", payload);
     });
 
     socket.on("ice-candidate", incoming => {
+        // console.log("ice-candidate");
         io.to(incoming.target).emit("ice-candidate", incoming.candidate);
     });
 });
